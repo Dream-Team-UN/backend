59b07cee62503a754f74eeee271db56c321b2214
unknown <sruizs.jr@gmail.com>
2020-10-15 19:53:07 -0500

    repositories

--- coronaMap/src/main/java/com/example/dataAccess/repositories/DepartamentoRepository.java
+++ coronaMap/src/main/java/com/example/dataAccess/repositories/DepartamentoRepository.java
@@ -1,5 +1,34 @@
 package com.example.dataAccess.repositories;
 
-public class DepartamentoRepository {
+import java.net.http.HttpResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.example.dataAccess.databases.ClientConnect;
+
+public class DepartamentoRepository {
+	
+	ClientConnect client = new ClientConnect();
+	
+	public JSONArray getAllDepartament() {
+		
+		return client.getClient().sendAsync(client.ClientRequest("?$select=distinct%20departamento"), HttpResponse.BodyHandlers.ofString())
+				.thenApply(HttpResponse::body).thenApply(DepartamentoRepository::getAllDepartament).join();
+	}
+	
+	public static JSONArray getAllDepartament(String responseBody) {
+		
+		JSONArray arrayDepartament = null;
+		try {
+			arrayDepartament = new JSONArray(responseBody);
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return arrayDepartament;
+		
+	}
+	
 }
--- coronaMap/src/main/java/com/example/dataAccess/repositories/MunicipioRepository.java
+++ coronaMap/src/main/java/com/example/dataAccess/repositories/MunicipioRepository.java
@@ -1,5 +1,32 @@
 package com.example.dataAccess.repositories;
 
+import java.net.http.HttpResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.example.dataAccess.databases.ClientConnect;
+
 public class MunicipioRepository {
+	
+	ClientConnect client = new ClientConnect();
+	
+public JSONArray getAllMunicipe() {
+		
+		return client.getClient().sendAsync(client.ClientRequest("?$select=distinct%20ciudad_de_ubicaci_n,%20departamento&$order=departamento"),
+				HttpResponse.BodyHandlers.ofString()).thenApply(HttpResponse::body).thenApply(MunicipioRepository::getAllMunicipe).join();
+	}
+	
+	public static JSONArray getAllMunicipe(String responseBody) {
+		
+		JSONArray arrayMunicipe = null;
+		try {
+			arrayMunicipe = new JSONArray(responseBody);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return arrayMunicipe;
+	}
 
 }
