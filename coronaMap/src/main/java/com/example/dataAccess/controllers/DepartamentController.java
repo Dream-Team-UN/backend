ef982948c6859e0bfa8a9c0b23a072ef116dc30a
Camilo Carrahza <30605048+ccarranzac@users.noreply.github.com>
2020-10-20 17:58:49 -0500

    aceptar peticiones de otros puertos

--- coronaMap/src/main/java/com/example/dataAccess/controllers/DepartamentController.java
+++ coronaMap/src/main/java/com/example/dataAccess/controllers/DepartamentController.java
@@ -3,12 +3,13 @@ package com.example.dataAccess.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dataAccess.models.entity.Departamento;
 import com.example.dataAccess.services.DepartamentService;
-
+@CrossOrigin(origins = { "http://localhost:8081" })
 @RestController
 public class DepartamentController {
 	
