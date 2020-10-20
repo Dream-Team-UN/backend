a122efabe05ff09da12ad0bcb0f31acb8d7f71be
tech <tech@Hideyoshi>
2020-10-19 21:44:03 -0500

    se crea controller de departamento

---
+++ coronaMap/src/main/java/com/example/dataAccess/models/InfoDepartamento.java
@@ -0,0 +1,69 @@
+package com.example.dataAccess.models;
+
+public class InfoDepartamento {
+	private int id;
+	private String CasosAct;
+	private String CasosTol;
+	private String CasosRec;
+	private String CasosFal;
+	private String CasosAsin;
+
+	
+	public InfoDepartamento(int id, String casosAct, String casosTol, String casosRec, String casosFal, String casosAsin) {
+		super();
+		this.id = id;
+		CasosAct = casosAct;
+		CasosTol = casosTol;
+		CasosRec = casosRec;
+		CasosFal = casosFal;
+		CasosAsin = casosAsin;
+	}
+
+	public String getCasosAsin() {
+		return CasosAsin;
+	}
+
+	public void setCasosAsin(String casosAsin) {
+		CasosAsin = casosAsin;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCasosAct() {
+		return CasosAct;
+	}
+
+	public void setCasosAct(String casosAct) {
+		CasosAct = casosAct;
+	}
+
+	public String getCasosTol() {
+		return CasosTol;
+	}
+
+	public void setCasosTol(String casosTol) {
+		CasosTol = casosTol;
+	}
+
+	public String getCasosRec() {
+		return CasosRec;
+	}
+
+	public void setCasosRec(String casosRec) {
+		CasosRec = casosRec;
+	}
+
+	public String getCasosFal() {
+		return CasosFal;
+	}
+
+	public void setCasosFal(String casosFal) {
+		CasosFal = casosFal;
+	}
+}
